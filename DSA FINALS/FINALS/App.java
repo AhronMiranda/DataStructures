6de560e77fdcd0d4d550c80ba7a23ec8e7221bef
AhronMiranda <ahronjieannekent.miranda@lorma.edu>
2026-05-22 13:42:24 +0800

    FINALS

--- DSA FINALS/FINALS/App.java
+++ DSA FINALS/FINALS/App.java
@@ -5,135 +5,128 @@ import java.util.Scanner;
 
 public class App {
 
-        //TYPE DESIGN
+        // TYPE DESIGN
         public static final String RED = "\033[0;31m";
         public static final String GREEN = "\033[0;32m";
         public static final String BLUE = "\033[0;34m";
         public static final String BOLD = "\u001B[1m";
         public static final String ITALIC = "\u001B[3m";
         public static final String RESET = "\033[0m";
-    public static void main(String[] args) {
-        int answer = 0;
-        boolean run = true;
 
-        Scanner sc = new Scanner(System.in);
-        while (run) {
-        try (FileWriter terminal = new FileWriter("RunProgram.txt", true)) {
-        System.out.println("""
-            
-                +---------------------------------------+
-                |   Welcome to Ahron's Code Systems     |
-                | ======================================|             
-                |                                       |
-                | Please choose one of the following:   |
-                | 1. EFM Grocery                        |    (1. PRELIMS)
-                | 2. Movie Registration                 |    (2. PRELIMS)
-                | 3. Movie Rental                       |    (3. PRELIMS)
-                | 4. Band Registration                  |    (4. PRELIMS NO FILE SAVING)
-                | 5. Extract Saved Bands                |    (5. MIDTERMS)
-                | 6. Musical Artists Menu               |    (6. MIDTERMS)
-                | 7. Quiz App                           |    (7. FINALS)
-                | 0. Exit                               |
-                +---------------------------------------+
-        
-                """);
+        public static void main(String[] args) {
+                int answer = 0;
+                boolean run = true;
 
-        terminal.write("""
-            
-                +---------------------------------------+
-                |   Welcome to Ahron's Code Systems     |
-                | ======================================|             
-                |                                       |
-                | Please choose one of the following:   |
-                | 1. EFM Grocery                        |    (1. PRELIMS)
-                | 2. Movie Registration                 |    (2. PRELIMS)
-                | 3. Movie Rental                       |    (3. PRELIMS)
-                | 4. Band Registration                  |    (4. PRELIMS NO FILE SAVING)
-                | 5. Extract Saved Bands                |    (5. MIDTERMS)
-                | 6. Musical Artists Menu               |    (6. MIDTERMS)
-                | 7. Quiz App                           |    (7. FINALS)
-                | 0. Exit                               |
-                +---------------------------------------+
-        
-                """);
-            
-            
-                System.out.print("User>> ");
+                Scanner sc = new Scanner(System.in);
+                while (run) {
+                        try (FileWriter terminal = new FileWriter("RunProgram.txt", true)) {
+                                System.out.println("""
 
-        
-            
-            
-                answer = inputInt(sc);
-                terminal.write("User>> " + answer + "\n");
-                
+                                                +---------------------------------------+
+                                                |   Welcome to Ahron's Code Systems     |
+                                                | ======================================|
+                                                |                                       |
+                                                | Please choose one of the following:   |
+                                                | 1. EFM Grocery                        |    (1. PRELIMS)
+                                                | 2. Movie Registration                 |    (2. PRELIMS)
+                                                | 3. Movie Rental                       |    (3. PRELIMS)
+                                                | 4. Band Registration                  |    (4. PRELIMS NO FILE SAVING)
+                                                | 5. Extract Saved Bands                |    (5. MIDTERMS)
+                                                | 6. Musical Artists Menu               |    (6. MIDTERMS)
+                                                | 7. Quiz App                           |    (7. FINALS)
+                                                | 0. Exit                               |
+                                                +---------------------------------------+
 
-        
+                                                """);
 
+                                terminal.write("""
 
-        } catch (IOException e) {
-                System.out.println(RED + "[ERROR FILE NOT FOUND]" + RESET);
+                                                +---------------------------------------+
+                                                |   Welcome to Ahron's Code Systems     |
+                                                | ======================================|
+                                                |                                       |
+                                                | Please choose one of the following:   |
+                                                | 1. EFM Grocery                        |    (1. PRELIMS)
+                                                | 2. Movie Registration                 |    (2. PRELIMS)
+                                                | 3. Movie Rental                       |    (3. PRELIMS)
+                                                | 4. Band Registration                  |    (4. PRELIMS NO FILE SAVING)
+                                                | 5. Extract Saved Bands                |    (5. MIDTERMS)
+                                                | 6. Musical Artists Menu               |    (6. MIDTERMS)
+                                                | 7. Quiz App                           |    (7. FINALS)
+                                                | 0. Exit                               |
+                                                +---------------------------------------+
+
+                                                """);
+
+                                System.out.print("User>> ");
+
+                                answer = inputInt(sc);
+                                terminal.write("User>> " + answer + "\n");
+
+                        } catch (IOException e) {
+                                System.out.println(RED + "[ERROR FILE NOT FOUND]" + RESET);
+                        }
+
+                        switch (answer) {
+
+                                case 1:
+                                        System.out.println("You have chosen EFM Grocery");
+                                        GroceryEFM grocery = new GroceryEFM();
+                                        grocery.runGroceryEFM();
+                                        break;
+                                case 2:
+                                        System.out.println("You have chosen Movie Registration");
+                                        MovieRegistration movie = new MovieRegistration();
+                                        movie.runMovieRegistration();
+                                        break;
+                                case 3:
+                                        System.out.println("You have chosen Movie Rental");
+                                        MovieRental movieRent = new MovieRental();
+                                        movieRent.runMovieRental();
+                                        break;
+                                case 4:
+                                        System.out.println("You have chosen Band Registration");
+                                        BandRegistration band = new BandRegistration();
+                                        band.runBandRegistration();
+                                        break;
+                                case 5:
+                                        System.out.println(GREEN + "Displaying Saved Bands\n" + RESET);
+                                        BandData data = new BandData();
+                                        data.runBandDataExtractionAndDisplay();
+                                        break;
+                                case 6:
+                                        MIDTERMS md = new MIDTERMS();
+                                        md.runMIDTERMS();
+                                        break;
+                                case 7:
+                                        QuizApp quiz = new QuizApp();
+                                        break;
+                                case 0:
+                                        System.out.println("Exiting...");
+                                        run = false;
+                                        break;
+                        }
+
+                }
+
+                System.out.println(GREEN + "Thank you for availing our services!!!" + RESET);
+
         }
 
-        
-        switch (answer) {
+        public static int inputInt(Scanner sc) {
+                while (true) {
+                        try {
+                                int tempInt = sc.nextInt();
+                                sc.nextLine();
+                                return tempInt;
+                        } catch (InputMismatchException e) {
+                                sc.nextLine();
+                                System.out.println(RED + "[INVALID INPUT]" + RESET);
+                        } catch (IndexOutOfBoundsException e) {
+                                sc.nextLine();
+                                System.out.println(RED + "[INVALID INPUT OUT OF BOUNDS]" + RESET);
+                        }
+                }
 
-        case 1 :
-                System.out.println("You have chosen EFM Grocery");
-                GroceryEFM grocery = new GroceryEFM();
-                grocery.runGroceryEFM();
-                break;
-        case 2 :
-                System.out.println("You have chosen Movie Registration");
-                MovieRegistration movie = new MovieRegistration();
-                movie.runMovieRegistration();
-                break;
-        case 3 :
-                System.out.println("You have chosen Movie Rental");
-                MovieRental movieRent = new MovieRental();
-                movieRent.runMovieRental();
-                break;
-        case 4 :
-                System.out.println("You have chosen Band Registration");
-                BandRegistration band = new BandRegistration();
-                band.runBandRegistration();
-                break;
-        case 5 :
-                System.out.println(GREEN + "Displaying Saved Bands\n" + RESET);
-                BandData data = new BandData();
-                data.runBandDataExtractionAndDisplay();
-                break;
-        case 6 :
-                MIDTERMS md = new MIDTERMS();
-                md.runMIDTERMS();
-                break;
-        case 7 :
-                QuizApp quiz = new QuizApp();
-                break;
-        case 0 :
-                System.out.println("Exiting...");
-                run = false;
-                break;
         }
-       
-    }
-
-    System.out.println(GREEN + "Thank you for availing our services!!!" + RESET);
-    
-    }
-
-    public static int inputInt(Scanner sc) {
-    while (true) {
-    try {
-    int tempInt = sc.nextInt(); sc.nextLine();
-    return tempInt;
-    } catch (InputMismatchException e) {
-    sc.nextLine();
-    System.out.println(RED + "[INVALID INPUT]" + RESET);
-    } catch (IndexOutOfBoundsException e) {
-    sc.nextLine();
-    System.out.println(RED + "[INVALID INPUT OUT OF BOUNDS]" + RESET);
-    }
-        }
-    
-    }
 }
--- DSA FINALS/FINALS/BandRegistration.java
+++ DSA FINALS/FINALS/BandRegistration.java
@@ -4,120 +4,114 @@ import java.util.Scanner;
 
 public class BandRegistration {
 
-        //SCANNER
-        public static Scanner sc = new Scanner(System.in);
+    // SCANNER
+    public static Scanner sc = new Scanner(System.in);
 
+    // TYPE DESIGN
+    public static final String RED = "\033[0;31m";
+    public static final String GREEN = "\033[0;32m";
+    public static final String BLUE = "\033[0;34m";
+    public static final String BOLD = "\u001B[1m";
+    public static final String ITALIC = "\u001B[3m";
+    public static final String RESET = "\033[0m";
 
-        //TYPE DESIGN
-        public static final String RED = "\033[0;31m";
-        public static final String GREEN = "\033[0;32m";
-        public static final String BLUE = "\033[0;34m";
-        public static final String BOLD = "\u001B[1m";
-        public static final String ITALIC = "\u001B[3m";
-        public static final String RESET = "\033[0m";
-
-    
     int iPerfID, iViewers;
     int count = 0;
     String strName, strGenre;
     double dStagePrice;
     char cUserAnswer = ' ';
 
-    public BandRegistration(){}
-
-    public void runBandRegistration() {
-    
-    
-    System.out.println(RED + BOLD + "Register your Band or Favorite Artist" + RESET);
-
-    System.out.println(GREEN + "=-=-=-=" + RESET + "\n");
-    
-    
-    do {
-    System.out.println(ITALIC + BLUE + "[Please Input the Following]" + RESET);
-    
-    while (true) {
-        try {
-    FileWriter terminal = new FileWriter("RunProgram.txt", true);
-    terminal.write("Prelim Band or Musical Artist Regiistration Chosen\n");
-
-
-    //STRINGS Name and Genre
-    System.out.print("Artist Name: ");
-    strName = inputString();
-    System.out.print("Genre: ");
-    strGenre = inputString();
-    
-    // INT Performance ID
-    System.out.print("PerformanceID: ");
-    iPerfID = inputInt();
-    
-    //DOUBLES Price of reserving the performer
-    System.out.print("Stage Price: ");
-    dStagePrice = inputDouble();
-
-
-    //INT Average viewers
-    System.out.print("Average Viewers: ");
-    iViewers = inputInt();
-    
-    print(strName, strGenre, iPerfID, dStagePrice, iViewers);
-
-    terminal.write(String.format("---REGISTERED THIS ARTIST OR BAND\nArtist Name: %s\nGenre: %s\nPerformanceID: %d\nStage Price: %.2f\nAverage Viewers: %d\n",
-    strName, strGenre, iPerfID, dStagePrice, iViewers ));
-
-
-    terminal.write("Musical Artist Added to the List [NO DATABASE SAVE]\n");
-    terminal.close();
-    break;
-    
-        } catch (IOException e) {
-            // YOU NEED TO RE-REGISTER IF NO FILE IS READ
-            System.out.println(RED + BOLD + "[ERROR] " + e.getMessage() + RESET);
-            System.out.print(BLUE + BOLD + "[TRY AGAIN FROM THE START]" + RESET + "\n");
-        } catch (Exception e) {
-
-            // YOU NEED TO RE-REGISTER IF YOU HAVE AN INVALID INPUT
-            System.out.println(RED + BOLD + "[ERROR] " + e.getMessage() + RESET);
-            System.out.print(BLUE + BOLD + "[TRY AGAIN FROM THE START]" + RESET + "\n");
-            sc.nextLine();
-        }
+    public BandRegistration() {
     }
 
-    count++;
-    System.out.println("---");
-    System.out.print("Enter another Y/N: ");
-    String strUserAnswer = inputString();
-    cUserAnswer = strUserAnswer.charAt(0);
-     System.out.println("---");
+    public void runBandRegistration() {
 
+        System.out.println(RED + BOLD + "Register your Band or Favorite Artist" + RESET);
 
-    } while (cUserAnswer == 'y' || cUserAnswer == 'Y');
+        System.out.println(GREEN + "=-=-=-=" + RESET + "\n");
 
-    
-    System.out.println(GREEN + count + " Music Artists registered today." + RESET);
-    
+        do {
+            System.out.println(ITALIC + BLUE + "[Please Input the Following]" + RESET);
+
+            while (true) {
+                try {
+                    FileWriter terminal = new FileWriter("RunProgram.txt", true);
+                    terminal.write("Prelim Band or Musical Artist Regiistration Chosen\n");
+
+                    // STRINGS Name and Genre
+                    System.out.print("Artist Name: ");
+                    strName = inputString();
+                    System.out.print("Genre: ");
+                    strGenre = inputString();
+
+                    // INT Performance ID
+                    System.out.print("PerformanceID: ");
+                    iPerfID = inputInt();
+
+                    // DOUBLES Price of reserving the performer
+                    System.out.print("Stage Price: ");
+                    dStagePrice = inputDouble();
+
+                    // INT Average viewers
+                    System.out.print("Average Viewers: ");
+                    iViewers = inputInt();
+
+                    print(strName, strGenre, iPerfID, dStagePrice, iViewers);
+
+                    terminal.write(String.format(
+                            "---REGISTERED THIS ARTIST OR BAND\nArtist Name: %s\nGenre: %s\nPerformanceID: %d\nStage Price: %.2f\nAverage Viewers: %d\n",
+                            strName, strGenre, iPerfID, dStagePrice, iViewers));
+
+                    terminal.write("Musical Artist Added to the List [NO DATABASE SAVE]\n");
+                    terminal.close();
+                    break;
+
+                } catch (IOException e) {
+                    // YOU NEED TO RE-REGISTER IF NO FILE IS READ
+                    System.out.println(RED + BOLD + "[ERROR] " + e.getMessage() + RESET);
+                    System.out.print(BLUE + BOLD + "[TRY AGAIN FROM THE START]" + RESET + "\n");
+                } catch (Exception e) {
+
+                    // YOU NEED TO RE-REGISTER IF YOU HAVE AN INVALID INPUT
+                    System.out.println(RED + BOLD + "[ERROR] " + e.getMessage() + RESET);
+                    System.out.print(BLUE + BOLD + "[TRY AGAIN FROM THE START]" + RESET + "\n");
+                    sc.nextLine();
+                }
+            }
+
+            count++;
+            System.out.println("---");
+            System.out.print("Enter another Y/N: ");
+            String strUserAnswer = inputString();
+            cUserAnswer = strUserAnswer.charAt(0);
+            System.out.println("---");
+
+        } while (cUserAnswer == 'y' || cUserAnswer == 'Y');
+
+        System.out.println(GREEN + count + " Music Artists registered today." + RESET);
 
     }
 
     public static String inputString() {
-    String tempString = sc.nextLine();
-    return tempString;
+        String tempString = sc.nextLine();
+        return tempString;
     }
 
     public static int inputInt() {
-    int tempInt = sc.nextInt();
-    sc.nextLine();
-    return tempInt;
+        int tempInt = sc.nextInt();
+        sc.nextLine();
+        return tempInt;
     }
 
     public static double inputDouble() {
-    double tempDouble = sc.nextDouble();
-    return tempDouble;
+        double tempDouble = sc.nextDouble();
+        return tempDouble;
     }
 
     public static void print(String strName, String strGenre, int iPerfID, double dStagePrice, int iViewers) {
-        System.out.println(RED + BOLD + "[REGISTER COMPLETE]\n" + RESET + "--- \nArtist Name: " + strName + " \nGenre: " + strGenre + " \nID: " + iPerfID + " \nReservation Price: " + dStagePrice + " \nAverage Viewers: " + iViewers);
-        
+        System.out.println(RED + BOLD + "[REGISTER COMPLETE]\n" + RESET + "--- \nArtist Name: " + strName + " \nGenre: "
+                + strGenre + " \nID: " + iPerfID + " \nReservation Price: " + dStagePrice + " \nAverage Viewers: "
+                + iViewers);
+
     }
 }
